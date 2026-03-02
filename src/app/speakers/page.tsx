3cebbc77cfce842e6b94652e3291871a76aea727
Dean Hull <125554301+Dean-Hull@users.noreply.github.com>
2026-03-02 20:54:43 +0000

    Fixed issue with footer jumping up the page

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -33,7 +33,7 @@ export default function Innovators() {
     const allTags = Object.values(SpeakerTag);
 
     return (
-        <main className="w-full h-full bg-slate-100 flex flex-col items-start justify-start space-y-8 mt-16">
+        <main className="w-full min-h-screen flex flex-col items-start justify-start space-y-8 mt-16">
             <div className="w-full max-w-7xl mx-auto justify-center gap-4 mt-16 px-4 mb-16">
                 <div className="flex flex-col items-start justify-start">
                     <h1 className="text-sm font-semibold text-black tracking-widest uppercase mb-4">
