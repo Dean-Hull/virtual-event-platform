4d12c21593134a7b99f3d6a3ebf5763efa29360e
Dean Hull <125554301+Dean-Hull@users.noreply.github.com>
2026-03-02 18:47:49 +0000

    Added placeholder home page content and text, removed unused imports

---
+++ src/app/home/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import HeroImage from '@/images/hero/conny-schneider-xuTJZ7uD7PI-unsplash.jpg';
+
+export default function Home() {
+    return (
+        <main className='relative w-screen h-screen overflow-hidden'>
+            <div className='absolute inset-0'>
+                <Image
+                    src={HeroImage}
+                    alt="Hero Image"
+                    fill
+                    priority
+                    className="object-cover" />
+                <div className='absolute inset-0 bg-black/50' />
+            </div>
+
+            { /* Content */ }
+            <div className='absolute inset-0 flex flex-col items-center justify-center space-y-8 px-6'>
+                <h1 className="text-4xl md:text-7xl font-bold tracking-tighter text-center text-white">
+                    Welcome to <span className="text-blue-500">LeadXP</span>
+                </h1>
+                <p className="text-lg text-white text-center w-full md:w-1/2 mx-auto font-light">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                </div>
+        </main>
+    );
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { navItems } from '@/constants/navItems';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { navItems } from '@/constants/navItems';
 
 interface NavItemProps {
     label: string;
@@ -12,7 +12,7 @@ interface NavItemProps {
 export default function Header() {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 h-16 bg-white shadow-md">
-            <div className='flex flex-row justify-between items-center h-full px-4 py-2'>
+            <div className='w-full max-w-7xl flex flex-row justify-between items-center h-full mx-auto'>
                 <h1 className="text-xl font-semibold tracking-tighter">LeadXP</h1>
                 <div className='uppercase text-sm font-semibold flex flex-row items-center tracking-tighter'>
                     {navItems.map((item => (
